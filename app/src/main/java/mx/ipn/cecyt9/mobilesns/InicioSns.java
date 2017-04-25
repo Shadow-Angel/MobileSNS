89e96f1d3c92b93338bb64fb6c70e14cdf3d0377
Shadow-Angel <mineangel1111@gmail.com>
2017-04-24 20:40:59 -0500

    Initial commit

---
+++ app/src/main/java/mx/ipn/cecyt9/mobilesns/InicioSns.java
@@ -0,0 +1,13 @@
+package mx.ipn.cecyt9.mobilesns;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InicioSns extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_sns);
+    }
+}
---
+++ app/src/main/java/mx/ipn/cecyt9/mobilesns/SNSLogin.java
@@ -0,0 +1,49 @@
+package mx.ipn.cecyt9.mobilesns;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+public class SNSLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        try {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_snslogin);
+        }catch (Exception e)
+        {
+            String Error;
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            Error = sw.toString();
+            Toast.makeText(getApplicationContext(), "Error: "+Error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void Iniciar(View v){
+        try {
+            Intent envia = new Intent(this, InicioSns.class);
+            Bundle datos = new Bundle();
+            envia.putExtras(datos);
+            finish();
+            startActivity(envia);
+        }
+        catch (Exception e)
+        {
+            String Error;
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            Error = sw.toString();
+            Toast.makeText(getApplicationContext(), "Error: "+Error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+}
