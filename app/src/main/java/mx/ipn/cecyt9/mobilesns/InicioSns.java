87cd711a03dcbda6ee2e65b7ec363cb5a9f3ca1d
Shadow-Angel <mineangel1111@gmail.com>
2017-04-30 18:47:07 -0500

    h

--- app/src/main/java/mx/ipn/cecyt9/mobilesns/InicioSns.java
+++ app/src/main/java/mx/ipn/cecyt9/mobilesns/InicioSns.java
@@ -1,7 +1,13 @@
 package mx.ipn.cecyt9.mobilesns;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 public class InicioSns extends AppCompatActivity {
 
@@ -10,4 +16,68 @@ public class InicioSns extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_sns);
     }
+
+    public void Usuarios(View v){
+        try
+        {
+            Intent envia = new Intent(this, UsuariosActivity.class);
+            Bundle datos = new Bundle();
+            envia.putExtras(datos);
+            finish();
+            startActivity(envia);
+        }
+        catch (Exception e)
+        {
+            String Error;
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            Error = sw.toString();
+            Toast.makeText(getApplicationContext(), "Error: "+Error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    public void Empleados(View v){
+        try{
+            Intent envia = new Intent(this, EmpleadosActivity.class);
+            Bundle datos = new Bundle();
+            datos.putString("NombreBuscado","");
+            envia.putExtras(datos);
+            finish();
+            startActivity(envia);
+        }
+        catch (Exception e)
+        {
+            String Error;
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            Error = sw.toString();
+            Toast.makeText(getApplicationContext(), "Error: "+Error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    public void Reportes(View v){
+        try {
+            Intent envia = new Intent(this, ReportActivity.class);
+            Bundle datos = new Bundle();
+            envia.putExtras(datos);
+            finish();
+            startActivity(envia);
+        }
+        catch (Exception e)
+        {
+            String Error;
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            Error = sw.toString();
+            Toast.makeText(getApplicationContext(), "Error: "+Error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+
 }
+
--- app/src/main/java/mx/ipn/cecyt9/mobilesns/SNSLogin.java
+++ app/src/main/java/mx/ipn/cecyt9/mobilesns/SNSLogin.java
@@ -1,10 +1,11 @@
 package mx.ipn.cecyt9.mobilesns;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
+
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
